55259e07b5dac03f34912c2dc3e69e4b97b84256
Lia Dias <79920078+lia-dias@users.noreply.github.com>
2022-12-11 19:20:32 +0000

    Add Maven para gestão de pacotes

---
+++ cestinha/src/test/java/com/github/lia-dias/cestinha/AppTest.java
@@ -0,0 +1,20 @@
+package com.github.lia-dias.cestinha;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
